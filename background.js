5f06c2b0fb76f1de6938384127b8040a5531af02
majesty17 <a-o@qq.com>
2017-08-10 16:55:09 +0800

    2

--- background.js
+++ background.js
@@ -6,9 +6,8 @@ crNode2();
 clearNode();
 
 
-
-function injectCustomJs(jsPath)
-{
+//注入js
+function injectCustomJs(jsPath){
     jsPath = jsPath || 'js/inject.js';
     var temp = document.createElement('script');
     temp.setAttribute('type', 'text/javascript');
@@ -23,16 +22,18 @@ function injectCustomJs(jsPath)
 	document.head.appendChild(cp);
 }
 
-
+//增加按钮
 function crNode2(){  
 	var oDivNode =document.getElementById('mark_top_bar')
-	oDivNode.innerHTML += "<button class='cp_btn'>Copy Text</button>";   
+	oDivNode.innerHTML += "<div class='mark-button-left'><button class='cp_btn button-mark button-red'>Copy Text</button></div>";   
 }  
 
+//清理一些无用node
 function clearNode(){
 	document.getElementsByClassName('container-head')[0].setAttribute('hidden','hidden');
 	document.getElementsByClassName('task-flow-wrap ng-scope')[0].setAttribute('hidden','hidden');
 	document.getElementsByClassName('pro-top-nav clearfix')[0].setAttribute('hidden','hidden');
+	document.getElementsByClassName('mark-top-bar-tips clearfix')[0].setAttribute('hidden','hidden');
 	
 	document.getElementsByClassName('item-x')[1].setAttribute('hidden','hidden');
 	document.getElementById('mark_right_rate_bar').setAttribute('hidden','hidden');
